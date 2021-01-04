9714b35357f907bc157571de87af9e98954bd4eb
João Pedro <joao.melquiades@ee.ufcg.edu.br>
2021-01-04 03:30:33 -0300

    Mudanças: * Método evaluate adicionado * Funções activation e d_activation adicionadas, que calculam a sigmoide e sua derivada, respectivamente

--- network.py
+++ network.py
@@ -8,6 +8,32 @@ para reconhecer dígitos manuscritos
 
 import numpy as np
 
+
+'''
+A função de ativação faz uma transformação
+no potencial de ativação u, transformando 
+uma combinação linear em algo não linear,
+o que permite utilizar do cálculo de derivadas
+para analisar o erro da rede. Nessa rede, 
+a função de ativação será a sigmoide, uma 
+função que tem imagem [0, 1].
+'''
+def activation(x):
+
+	return 1.0/(1 + np.exp(-x))
+
+'''
+A d_activation calcula a derivada da
+função de ativação, isso será necessário
+quando o algoritmo de backpropagation
+for implementado
+'''
+
+def d_activation(x):
+	return activation(x)*(1.0 - activation(x))
+
+
+
 class Network:
 
 	'''
@@ -39,4 +65,23 @@ class Network:
 			self.W.append(np.random.randn(x, y))
 
 
-	
+	'''
+	O método estimate irá calcular uma saída para uma determinada entrada.
+	Essa entrada deve ser um array no formato (n_i, 1), sendo n_i o número
+	de nós no input layer
+	'''
+	def estimate(self, x):
+		u = []
+		g = x
+
+		for b, w in zip(self.B, self.W):
+			u = np.dot(w*g) + b
+			g = activation(u)
+		return g
+
+
+	'''
+	O próximo método a ser implementado é o método de backpropagation. 
+	Esse método irá retornar as derivadas parciais de cada saída referente
+	a todas as entradas.
+	'''
